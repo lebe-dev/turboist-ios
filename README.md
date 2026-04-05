38edd209769a9d6130e6561dd01fff44df32f54c
Eugene Lebedev <duke.tougu@gmail.com>
2026-04-05 19:56:53 +0300

    Project skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+# Turboist (iOS App)
---
+++ turboistTests/turboistTests.swift
@@ -0,0 +1,19 @@
+//
+//  turboistTests.swift
+//  turboistTests
+//
+//  Created by Eugene Lebedev on 05/04/2026.
+//
+
+import Testing
+@testable import turboist
+
+struct turboistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
